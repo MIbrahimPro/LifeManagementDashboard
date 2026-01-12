8d726ebbf92002e53fa4ed00c8984f707a93fa13
Muhammad Ibrahim <mibrahimpro.1@gmail.com>
2026-01-13 01:15:15 +0500

    ui changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        tailwindcss(), ,
+        tailwindcss({
+            config: {
+                content: [
+                    './index.html',
+                    './src/**/*.{js,ts,jsx,tsx}',
+                ],
+                darkMode: '[data-mode="dark"]',
+                theme: {
+                    extend: {},
+                },
+                plugins: [],
+            }
+        }),
         react()
     ],
 })
