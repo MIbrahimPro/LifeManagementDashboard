660045512751eafacba39c4b3dab398b5b2879f2
Muhammad Ibrahim <mibrahimpro.1@gmail.com>
2026-01-13 01:18:59 +0500

    ui changes plus error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        tailwindcss({
-            content: [
-                './index.html',
-                './src/**/*.{js,ts,jsx,tsx}',
-            ],
-            darkMode: '[data-mode="dark"]',
-            theme: {
-                extend: {},
-            },
-            plugins: [],
-        }),
+        tailwindcss(),
         react()
     ],
 })
